f0804619452ef30f7acffc64431aa90841a3788b
him8033 <himanshu8033@gmail.com>
2024-11-26 15:12:02 +0530

    Create collections schema and also insert data one by one

--- index.js
+++ index.js
@@ -8,6 +8,31 @@ main()
         console.log(err);
     });
 
-async function main(){
+async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+});
+
+//      Model in Mongoose is a class with which we constructs documents.here  Collection name is given "Users" but actully store in mongodb is "users"
+
+const User = mongoose.model("User", userSchema);
+const user2 = new User({
+    name: "Eve",
+    email: "eve@gmail.com",
+    age: 21
+})
+
+//      Insert data or documents in collection one by one
+
+user2.save()
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
