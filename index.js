edf461140e6e9d655ddb3675a609e1f08fe5d603
him8033 <himanshu8033@gmail.com>
2024-11-27 19:09:23 +0530

    Delete method and their types

--- index.js
+++ index.js
@@ -65,10 +65,23 @@ const user2 = new User({
 
 //      ****************************            Update Method How to update the documents in collection
 
-// User.updateOne({name: "Tony"},{age:30})         //Update method with single document
-// User.updateMany({age:{$gt:25}},{age:40})            //Update method with multiple documents
-// User.findOneAndUpdate({name: "Tony"},{age:50})          //Update method with showing data in this method first show Old data and then Update
-User.findOneAndUpdate({name: "Tony"},{age:45},{new:true})          //In this method first update data and then show new updated document value
+// // User.updateOne({name: "Tony"},{age:30})         //Update method with single document
+// // User.updateMany({age:{$gt:25}},{age:40})            //Update method with multiple documents
+// // User.findOneAndUpdate({name: "Tony"},{age:50})          //Update method with showing data in this method first show Old data and then Update
+// User.findOneAndUpdate({name: "Tony"},{age:45},{new:true})          //In this method first update data and then show new updated document value
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+
+//              ****************************            Delete Method how to delete documents in mongodb
+
+// User.deleteOne({ name: "Ew" })          //Delete Method with Single documents only data deleted not print the deleted data
+// User.deleteMany({age:{$gt:23}})             //Delete Method with Multiple documents only data deleted not print the deleted data
+// User.findByIdAndDelete("67459541eac7f888b605b97a")          //Delete method with id and also ptint the data who are deleted
+User.findOneAndDelete({name:"Eve"})         //Delete data with any condition and also print the deleted data
     .then((res) => {
         console.log(res);
     })
