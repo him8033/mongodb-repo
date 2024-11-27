0c51a901cbcd60524ecb472e6161205dc20044d4
him8033 <himanshu8033@gmail.com>
2024-11-27 18:54:46 +0530

    Update method with their multiple types

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
     age: Number
 });
 
-//      Model in Mongoose is a class with which we constructs documents.here  Collection name is given "Users" but actully store in mongodb is "users"
+//          ****************************         Model in Mongoose is a class with which we constructs documents.here  Collection name is given "Users" but actully store in mongodb is "users"
 
 const User = mongoose.model("User", userSchema);
 const user2 = new User({
@@ -27,7 +27,7 @@ const user2 = new User({
     age: 21
 })
 
-//      Insert data or documents in collection one by one
+//          ****************************         Insert data or documents in collection one by one
 
 // user2.save()
 //     .then((res) => {
@@ -37,7 +37,7 @@ const user2 = new User({
 //         console.log(err);
 //     })
 
-//      Insert bulk data with "insertMany" method
+//          ****************************          Insert bulk data with "insertMany" method
 
 // User.insertMany([
 //     { name: "Tony", email: "tony@gmail.com", age: 22},
@@ -47,17 +47,30 @@ const user2 = new User({
 //     console.log(res);
 // });
 
-//      Find Method implement 
+//          ****************************             Find Method implement 
 
-User.find({})                    //find method without any condition
-// User.find({age:{$gt:22}})          //find method with condition
-// User.findOne({age:{$gt:22}})        //find method with findOne command for searching one data
-// User.find({_id:"674598ac01ea8d8eabb5820b"})         //find data with id one another "findById" also implemented
-// User.findById("674598ac01ea8d8eabb5820b")           //find data with "findById" method
+// User.find({})                    //find method without any condition
+// // User.find({age:{$gt:22}})          //find method with condition
+// // User.findOne({age:{$gt:22}})        //find method with findOne command for searching one data
+// // User.find({_id:"674598ac01ea8d8eabb5820b"})         //find data with id one another "findById" also implemented
+// // User.findById("674598ac01ea8d8eabb5820b")           //find data with "findById" method
+//     .then((res) => {
+//         console.log(res);       //print all the documents as a array 
+//         console.log(res[0]);        //print documents as their index that are stored in these array
+//         console.log(res[0].name);       //print particular object value in their document
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+
+//      ****************************            Update Method How to update the documents in collection
+
+// User.updateOne({name: "Tony"},{age:30})         //Update method with single document
+// User.updateMany({age:{$gt:25}},{age:40})            //Update method with multiple documents
+// User.findOneAndUpdate({name: "Tony"},{age:50})          //Update method with showing data in this method first show Old data and then Update
+User.findOneAndUpdate({name: "Tony"},{age:45},{new:true})          //In this method first update data and then show new updated document value
     .then((res) => {
-        console.log(res);       //print all the documents as a array 
-        console.log(res[0]);        //print documents as their index that are stored in these array
-        console.log(res[0].name);       //print particular object value in their document
+        console.log(res);
     })
     .catch((err) => {
         console.log(err);
